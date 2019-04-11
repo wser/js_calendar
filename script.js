cf01799cac96941932d142bfa1815bbf167a5f27
wser <w.web.dev@gmail.com>
2019-04-11 14:10:26 +0200

    first meaningfull month

--- script.js
+++ script.js
@@ -23,11 +23,6 @@ const months = [
   'December'
 ];
 
-Date.prototype.getCurrentDate = function() {
-  // Returns date in the format yyyy-mm-dd
-  return d.toLocaleDateString(localeStr);
-};
-
 Date.prototype.getWeekNumber = function() {
   let d = new Date(
     Date.UTC(this.getFullYear(), this.getMonth(), this.getDate())
@@ -58,30 +53,30 @@ function domText(id, val) {
   ccontainer.appendChild(item);
 }
 
-let yv = 2019,
-  mv = 5,
-  dv = 1;
+// let yv = 2019,
+//   mv = 5,
+//   dv = 1;
 
-let dt = `${yv}, ${mv}, ${dv}`;
-// let dt = new Date();
+// let dt = `${yv}, ${mv}, ${dv}`;
+// // let dt = new Date();
 
-let d = new Date(dt);
-let y = d.getFullYear();
-let m = d.getMonth();
+// let d = new Date(dt);
+// let y = d.getFullYear();
+// let m = d.getMonth();
 
-let dayInWeek = weekdays[d.getDay()]; //string
-let monthName = months[m];
-let weeknum = parseInt(d.getWeekNumber()); //int // get week from date
-let monday = parseInt(d.getMonday().getDate()); //int
-let monMonth = parseInt(d.getMonday().getMonth() + 1); //int
-let year = parseInt(1900 + d.getYear()); //int
-let firstInYearDate = new Date(y, 0, 1);
-let currentDate = d.getCurrentDate(); //string
-let firstMDay = new Date(y, m, 1).getDate();
-let eoThisMonth = parseInt(new Date(y, m + 1, 0).getDate()); //int
-let eoPrevMonth = parseInt(new Date(y, m, 0).getDate()); //int
+// let dayInWeek = weekdays[d.getDay()]; //string
+// let monthName = months[m];
+// let weeknum = parseInt(d.getWeekNumber()); //int // get week from date
+// let monday = parseInt(d.getMonday().getDate()); //int
+// let monMonth = parseInt(d.getMonday().getMonth() + 1); //int
+// let year = parseInt(1900 + d.getYear()); //int
+// let firstInYearDate = new Date(y, 0, 1);
+// let currentDate = d.toLocaleDateString(localeStr); //string
+// let firstMDay = new Date(y, m, 1).getDate();
+// let eoThisMonth = parseInt(new Date(y, m + 1, 0).getDate()); //int
+// let eoPrevMonth = parseInt(new Date(y, m, 0).getDate()); //int
 
-const firstInWeekNmbr = function(week, y) {
+const mondayFromWeekNumber = function(week, y) {
   // sets default year to current if undefined
   y = typeof y !== 'undefined' ? y : new Date().getFullYear();
   // sets date of first in year
@@ -103,61 +98,61 @@ const firstInWeekNmbr = function(week, y) {
   return new Date(fyD.getTime() + noOfWeeks - days);
 };
 
-// console.log(parseInt(firstInWeekNmbr(1).getDate()));
+// console.log(parseInt(mondayFromWeekNumber(1).getDate()));
 
 const wCal = {
-  calendarHelp() {
-    domText('fulldate', 'fulldate: ' + currentDate);
-    domText('weekday', 'weekday: ' + dayInWeek);
-    domText('weeknum', 'weeknum: ' + weeknum);
-    domText('monday', 'monday: ' + monday + '.' + monMonth);
-    domText('month', 'month: ' + monthName);
-    domText('eoThisMonth', 'end of month: ' + eoThisMonth);
-    domText('eoprevmonth', 'end of  prev month: ' + eoPrevMonth);
-    domText('year', 'year: ' + year);
-    //append to DOM div
-    document.getElementsByClassName('calendarHelp')[0].appendChild(ccontainer);
-  },
+  // calendarHelp() {
+  //   domText('fulldate', 'fulldate: ' + currentDate);
+  //   domText('weekday', 'weekday: ' + dayInWeek);
+  //   domText('weeknum', 'weeknum: ' + weeknum);
+  //   domText('monday', 'monday: ' + monday + '.' + monMonth);
+  //   domText('month', 'month: ' + monthName);
+  //   domText('eoThisMonth', 'end of month: ' + eoThisMonth);
+  //   domText('eoprevmonth', 'end of  prev month: ' + eoPrevMonth);
+  //   domText('year', 'year: ' + year);
+  //   //append to DOM div
+  //   document.getElementsByClassName('calendarHelp')[0].appendChild(ccontainer);
+  // },
   createWeek() {
-    let x = firstInWeekNmbr(1, 2019);
-    // let xd = new Date(x.getFullYear(), x.getMonth(), x.getDate()).getDate();
-    let xd = x.getDate();
+    for (let j = 0; j < 5; j++) {
+      let x = mondayFromWeekNumber(j + 1, 2019);
 
-    console.log(x.getFullYear());
-    console.log(x.getMonth());
-    console.log(x.getDate());
-    console.log(xd);
+      //Set the parent element
+      let week = document.createElement('div');
+      week.className = 'week';
+      week.id = `week_${j + 1}`;
 
-    //Set the parent element
-    let week = document.createElement('div');
-    week.className = 'week';
-    week.id = `week_${1}`;
+      // Create the inner elems before appending to parent
+      for (let i = 0; i < 8; i++) {
+        //Set the child element
+        let cell = document.createElement('div');
+        cell.className = 'cell';
 
-    // Create the inner elems before appending to parent
-    for (let i = 0; i < 8; i++) {
-      //Set the child element
-      let day = document.createElement('div');
-      day.className = 'day';
+        let xd = new Date(x.getFullYear(), x.getMonth(), x.getDate() + i - 1);
+        let xxd = xd.toLocaleDateString(localeStr);
+        let xxxd = xd.getDate();
 
-      if (i == 0) {
-        day.id = `${i + 1}`;
-        day.innerHTML = i + 1;
-      } else {
-        // let val = (xd + 1).getCurrentDate();
-        // day.id = `${val}`;
-        // day.innerHTML = val;
+        if (i == 0) {
+          cell.id = `week#${j + 1}`;
+          cell.innerHTML = j + 1;
+          cell.style =
+            'text-align: center; font-size:24px; color:white; background:#4abf8a';
+        } else {
+          cell.id = `${xxd}`;
+          cell.innerHTML = xxxd;
+        }
+
+        // append to parent
+        week.appendChild(cell);
       }
-
-      // append to parent
-      week.appendChild(day);
+      //append to DOM div
+      document.getElementsByClassName('calendar')[0].appendChild(week);
     }
-    //append to DOM div
-    document.getElementsByClassName('calendar')[0].appendChild(week);
   }
 };
 
 window.onload = function() {
   //call calendar() when page load
-  wCal.calendarHelp();
+  // wCal.calendarHelp();
   wCal.createWeek();
 };
