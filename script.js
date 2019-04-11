5dd87243e1a4dec1f807306388ecd2aed911715a
wser <w.web.dev@gmail.com>
2019-04-11 15:13:59 +0200

    code refactoring

--- script.js
+++ script.js
@@ -23,44 +23,42 @@ const months = [
   'December'
 ];
 
-const getWeekNumber = function(d) {
-  // Copy date so don't modify original
-  d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
-  // Set to nearest Thursday: current date + 4 - current day number
-  // Make Sunday's day number 7
-  d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay() || 7));
-  // Get first day of year
-  var yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
-  // Calculate full weeks to nearest Thursday
-  return Math.ceil(((d - yearStart) / 86400000 + 1) / 7);
-};
-
-const mondayFromWeekNumber = function(week, y) {
-  // sets default year to current if undefined
-  y = typeof y !== 'undefined' ? y : new Date().getFullYear();
-  // sets date of first in year
-  let fyD = new Date(y, 0, 1);
-  // gets day of week
-  let fyInWeek = fyD.getDay();
-
-  // multiplies with days in correspondence to day in week of first year date
-  // to get a correct weekday number at the beginning of a year
-  let res = 1;
-  if (fyInWeek > 4) res = 7;
-  if (fyInWeek == 6) res = 18;
-
-  // number of requested weeks in miliseconds
-  let noOfWeeks = 604800000 * (week - 1);
-  // number of days in miliseconds
-  let days = 3600000 * 24 * res * (fyInWeek - 1);
-  // returns start of year plus nuOfweeks minus days in week
-  return new Date(fyD.getTime() + noOfWeeks - days);
-};
-
 const wCal = {
+  getWeekNumber(d) {
+    // Copy date so don't modify original
+    d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
+    // Set to nearest Thursday: current date + 4 - current day number
+    // Make Sunday's day number 7
+    d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay() || 7));
+    // Get first day of year
+    var yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
+    // Calculate full weeks to nearest Thursday
+    return Math.ceil(((d - yearStart) / 86400000 + 1) / 7);
+  },
+  mondayFromWeekNumber(week, y) {
+    // sets default year to current if undefined
+    y = typeof y !== 'undefined' ? y : new Date().getFullYear();
+    // sets date of first in year
+    let fyD = new Date(y, 0, 1);
+    // gets day of week
+    let fyInWeek = fyD.getDay();
+
+    // multiplies with days in correspondence to day in week of first year date
+    // to get a correct weekday number at the beginning of a year
+    let res = 1;
+    if (fyInWeek > 4) res = 7;
+    if (fyInWeek == 6) res = 18;
+
+    // number of requested weeks in miliseconds
+    let noOfWeeks = 604800000 * (week - 1);
+    // number of days in miliseconds
+    let days = 3600000 * 24 * res * (fyInWeek - 1);
+    // returns start of year plus nuOfweeks minus days in week
+    return new Date(fyD.getTime() + noOfWeeks - days);
+  },
   createCal() {
     for (let j = 0; j < 5; j++) {
-      let x = mondayFromWeekNumber(j + 1, 2019);
+      let x = this.mondayFromWeekNumber(j + 1, 2019);
 
       //Set the parent element
       let week = document.createElement('div');
@@ -77,7 +75,7 @@ const wCal = {
         let xxd = xd.toLocaleDateString(localeStr);
         let xxxd = xd.getDate();
         let xdd = new Date(x.getFullYear(), x.getMonth(), x.getDate() + i);
-        let wkNum = getWeekNumber(xdd);
+        let wkNum = this.getWeekNumber(xdd);
         //console.log(wkNum);
 
         if (i == 0) {
