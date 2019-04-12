fe57e330f733539da0cfe01589d1c9ef78c41a08
wser <w.web.dev@gmail.com>
2019-04-12 13:08:44 +0200

    fixed header

--- script.js
+++ script.js
@@ -1,65 +1,51 @@
 const localeStr = 'sv-SE';
 const year = 2019;
-const displayWeeks = 50;
+const displayWeeks = 70;
 
 const weekdays = [
-  'Sunday',
-  'Monday',
-  'Tuesday',
-  'Wednesday',
-  'Thursday',
-  'Friday',
-  'Saturday'
+  'wk',
+  'Mon',
+  'Tue',
+  'Wed',
+  'Thu',
+  'Fri',
+  'Sat',
+  'Sun',
+  'mth, year'
 ];
 const months = [
-  'January',
-  'February',
-  'March',
-  'April',
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
   'May',
-  'June',
-  'July',
-  'August',
-  'September',
-  'October',
-  'November',
-  'December'
+  'Jun',
+  'Jul',
+  'Aug',
+  'Sep',
+  'Oct',
+  'Nov',
+  'Dec'
 ];
 
 const wCal = {
   getWeekNumber(d) {
-    // Copy date so don't modify original
-    d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
-    // Set to nearest Thursday: current date + 4 - current day number
-    // Make Sunday's day number 7
-    d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay() || 7));
-    // // Get first day of year
-    // var yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
-    // Calculate full weeks to nearest Thursday
+    d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate())); // Copy date so don't modify original
+    d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay() || 7)); // Set to nearest Thursday: current date + 4 - current day number // Make Sunday's day number 7
     return Math.ceil(
-      ((d - new Date(Date.UTC(d.getUTCFullYear(), 0, 1))) / 86400000 + 1) / 7
+      ((d - new Date(Date.UTC(d.getUTCFullYear(), 0, 1))) / 86400000 + 1) / 7 // Calculate full weeks to nearest Thursday
     );
   },
   mondayFromWeekNumber(week, y) {
-    // sets default year to current if undefined
-    y = typeof y !== 'undefined' ? y : new Date().getFullYear();
-    // sets date of first in year
-    let fyD = new Date(y, 0, 1);
-    // gets day of week
-    let fyInWeek = fyD.getDay();
-
-    // multiplies with days in correspondence to day in week of first year date
-    // to get a correct weekday number at the beginning of a year
-    let res = 1;
+    y = typeof y !== 'undefined' ? y : new Date().getFullYear(); // sets default year to current if undefined
+    let fyD = new Date(y, 0, 1); // sets date of first in year
+    let fyInWeek = fyD.getDay(); // gets day of week
+    let res = 1; // multiplier to get a correct weekday number at the beginning of a year
     if (fyInWeek > 4) res = 7;
     if (fyInWeek == 6) res = 18;
-
-    // number of requested weeks in miliseconds
-    let noOfWeeks = 604800000 * (week - 1);
-    // number of days in miliseconds
-    let days = 3600000 * 24 * res * (fyInWeek - 1);
-    // returns start of year plus nuOfweeks minus days in week
-    return new Date(fyD.getTime() + noOfWeeks - days);
+    let noOfWeeks = 604800000 * (week - 1); // number of requested weeks in miliseconds
+    let days = 3600000 * 24 * res * (fyInWeek - 1); // number of days in miliseconds
+    return new Date(fyD.getTime() + noOfWeeks - days); // returns start of year plus nuOfweeks minus days in week
   },
   createCal() {
     //Set the parent element
@@ -70,20 +56,29 @@ const wCal = {
       //Set the child element
       let week = [];
       // Create the inner elems before appending to parent
-      for (let i = 0; i < 8; i++) {
+      for (let i = 0; i < 9; i++) {
         let xd = new Date(x.getFullYear(), x.getMonth(), x.getDate() + i - 1);
 
         let wkNum = this.getWeekNumber(
           new Date(x.getFullYear(), x.getMonth(), x.getDate() + i)
         );
-        //console.log(wkNum);
+
+        let month = new Date(x.getFullYear(), x.getMonth(), x.getDate() + 6);
 
         if (i == 0) {
-          let wk = x.getFullYear() + '-' + wkNum;
           week.push(`<div 
               class="cell" 
-              id="week#${wk}" 
-              style="font-size:18px; color:white; background:#4abf8a">${wk}</div>`);
+              id="week#${wkNum}" 
+              style="font-size:18px; color:white; background:#4abf8a">${wkNum}</div>`);
+        } else if (i == 8) {
+          week.push(`<div 
+              class="cell" 
+              id="month#${months[month]}" 
+              style="color:white; background:#4abf8a">${months[
+                month.getMonth()
+              ] +
+                ', ' +
+                month.getFullYear()}</div>`);
         } else {
           week.push(`<div 
               class="cell" 
@@ -93,8 +88,10 @@ const wCal = {
       calendar.push(
         `<div class="week" id="week_${j + 1}" >${week.join('')}</div>`
       );
+
+      let content = calendar.join('');
       //append to DOM div
-      document.querySelector('.calendar').innerHTML = calendar.join('');
+      document.querySelector('.calendar').innerHTML = content;
     }
     document.getElementById('loading').style.display = 'none';
   }
@@ -104,5 +101,18 @@ const wCal = {
 window.onload = function() {
   var timerStart = Date.now();
   wCal.createCal();
+  wkDays = [];
+  for (let i = 0; i < weekdays.length; i++) {
+    wkDays.push(`<div 
+              class="cell" 
+              id="day#${weekdays[i]}" 
+              style="color:white; background:#4abf8a">${weekdays[i]}</div>`);
+  }
+  document
+    .querySelector('.calendar')
+    .insertAdjacentHTML(
+      'afterbegin',
+      `<div class="week header">${wkDays.join('')}</div>`
+    );
   console.log('Load time: ', (Date.now() - timerStart) / 1000, 'seconds');
 };
